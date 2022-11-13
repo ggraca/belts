b8965d668beb9a5b1c882deb4f99c521b9692aac
Guilherme Graca <ggracase@gmail.com>
2022-11-13 16:58:09 +0000

    Keep buffer references

--- belts_bgfx/lib/belts_bgfx/assets/mesh_loader.rb
+++ belts_bgfx/lib/belts_bgfx/assets/mesh_loader.rb
@@ -3,8 +3,11 @@ module BeltsBGFX::Assets
     attr_reader :vah, :vbh, :ibh
 
     def initialize(vertices, indexes)
-      @vertices = vertices
-      @indexes = indexes
+      @vert_buffer = FFI::MemoryPointer.new(:float, vertices.size)
+      @vert_buffer.write_array_of_float(vertices)
+
+      @index_buffer = FFI::MemoryPointer.new(:uint16, indexes.size)
+      @index_buffer.write_array_of_type(:uint16, :put_uint16, indexes)
 
       @vah = BGFX::VertexLayout.new
       BGFX.vertex_layout_begin(@vah, 0)
@@ -17,16 +20,10 @@ module BeltsBGFX::Assets
     end
 
     def load
-      vert_buffer = FFI::MemoryPointer.new(:float, @vertices.size)
-      vert_buffer.write_array_of_float(@vertices)
-
-      vertex_ref = BGFX.make_ref(vert_buffer, vert_buffer.size)
+      vertex_ref = BGFX.make_ref(@vert_buffer, @vert_buffer.size)
       @vbh = BGFX.create_vertex_buffer(vertex_ref, @vah, 0)
 
-      index_buffer = FFI::MemoryPointer.new(:uint16, @indexes.size)
-      index_buffer.write_array_of_type(:uint16, :put_uint16, @indexes)
-
-      index_ref = BGFX.make_ref(index_buffer, index_buffer.size)
+      index_ref = BGFX.make_ref(@index_buffer, @index_buffer.size)
       @ibh = BGFX.create_index_buffer(index_ref, 0)
     end
 
